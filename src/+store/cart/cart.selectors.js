28d558865fbf45f32c096cf19d951e8e8d7198a5
Martin Metodiev <martin.metodiew@gmail.com>
2022-02-02 21:42:19 +0200

    feat: update cart quantity dynamically & animate on each added item

--- src/+store/cart/cart.selectors.js
+++ src/+store/cart/cart.selectors.js
@@ -1,3 +1,9 @@
 export const getCartDropdownIsHidden = state => state.cart.hidden;
 
 export const getCartItems = state => state.cart.cartItems;
+
+export const getCartItemsQuantity = state =>
+  state.cart.cartItems.reduce(
+    (accumulatedQuantity, cartItem) => accumulatedQuantity + cartItem.quantity,
+    0
+  );
--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -1,23 +1,42 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { useDispatch } from 'react-redux';
+import { useSelector } from 'react-redux';
 
 import './cart-icon.styles.scss';
 import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
 import { toggleCartDropdown } from '../../+store/cart/cart.actions';
+import { getCartItemsQuantity } from '../../+store/cart/cart.selectors';
 
 const CartIcon = () => {
+  const [animate, setAnimate] = useState(false);
+  const isMounting = useRef(true);
   const dispatch = useDispatch();
+  const quantity = useSelector(getCartItemsQuantity);
 
-  const toggleCartDropdownHandler =() => {
+  useEffect(() => {
+    if (isMounting.current) {
+      isMounting.current = false; //No animation on initial mount
+    } else {
+      setAnimate(true); // Set animate to true
+    }
+  }, [quantity]); //Only run useEffect if quantity has changed
+
+  const toggleCartDropdownHandler = () => {
     dispatch(toggleCartDropdown());
-  }
-  
+  };
+
+  const classes = `cart-icon ${animate ? 'animate-icon' : ''}`;
+
   return (
-    <div className="cart-icon" onClick={toggleCartDropdownHandler}>
+    <div
+      className={classes}
+      onClick={toggleCartDropdownHandler}
+      onAnimationEnd={() => setAnimate(false)}
+    >
       <ShoppingIcon className="shopping-icon" />
-      <span className="item-count">0</span>
+      <span className="item-count">{quantity}</span>
     </div>
   );
-};
+};;
 
 export default CartIcon;
